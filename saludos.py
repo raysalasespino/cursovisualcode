1819ada41f586441f784956c98569d94545440f0
Ray Salas Espino <csalas27932@hotmail.com>
2021-10-13 10:43:02 -0500

    se añaden dos nuevos archivos

--- saludos.py
+++ saludos.py
@@ -1,3 +1,9 @@
 saludo = "hola curso"
-
-print(saludo)
+numero1 = 10
+numero2 = 30
+suma = numero1 + numero2
+resta = numero1 - numero2
+print(saludo) 
+print ("El resultado de la suma es ", suma) 
+print ("El resultado de la resta es ", resta)  
+print(saludo)   
